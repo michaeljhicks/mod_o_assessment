390af3404b583d8f8465b1856643c71e397fdb08
Michael Hicks <hicks.michael04@gmail.com>
2021-10-22 21:15:27 -0600

    Add two of each varible using a hash

---
+++ model_b.rb
@@ -0,0 +1,11 @@
+vehicle = {
+  "vehicle_name" => 'The Turing Touring',
+  "vehicle_type" => 'All Terrain Touring SUV',
+  "vehicle_size" => 'Very large',
+  "num_of_cupholders" => 19,
+  "num_of_wheels" => 5,
+  "car_of_the_year" => true,
+  "good_gas_mileage" => false,
+  "vehicle_colors" => ['white', 'black', 'graphite pear', 'eggplant purple', 'trump orange'],
+  "vehicle_packages" => ['standard', 'sport', 'premium', 'student', 'graduate', 'instructor' ]
+}
