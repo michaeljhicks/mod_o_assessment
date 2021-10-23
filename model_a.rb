830d0ca927449594fdfe5b16e2dd806352e02a21
Michael Hicks <hicks.michael04@gmail.com>
2021-10-22 21:04:36 -0600

    Add two of each variable

---
+++ model_a.rb
@@ -0,0 +1,11 @@
+airport = {
+  "name" => 'Alan Turing International',
+  "best_restaurant" => 'Root Down',
+  "worst_restaurant" => 'Hudson News',
+  "num_of_gates" => 84,
+  "num_of_concourses" => 5,
+  "happy_travelers" => true,
+  "happy_mother_in_law" => false,
+  "southwest_pilots" => ['kevin', 'dr. pepper', 'sgt. pepper', "suzanne"],
+  "parking_lots" => ['british double decker lot', 'royale with cheese lot', 'pikes peak lot', 'brexit lot'],
+}
