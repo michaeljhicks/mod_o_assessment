7f324ba88c9ad6f2a25c9e5b086085235c220382
Michael Hicks <hicks.michael04@gmail.com>
2021-10-22 20:47:17 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Senator Ted Cruz"
+is_active = true
+famous_person = "Madonna"
+num_3 = 52
+num_9 = 26
+sum = (num_3 + num_9)
+animals = ["aardvark", "ragdoll cat", "anaconda"]
+animals.shift()
+puts famous_person.length
