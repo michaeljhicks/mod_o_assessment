0af92ec6719fb0ef1cbb3faac4d6198a19247700
Michael Hicks <hicks.michael04@gmail.com>
2021-10-22 21:14:46 -0600

    Remove final comma

--- model_a.rb
+++ model_a.rb
@@ -7,5 +7,5 @@ airport = {
   "happy_travelers" => true,
   "happy_mother_in_law" => false,
   "southwest_pilots" => ['kevin', 'dr. pepper', 'sgt. pepper', "suzanne"],
-  "parking_lots" => ['british double decker lot', 'royale with cheese lot', 'pikes peak lot', 'brexit lot'],
+  "parking_lots" => ['british double decker lot', 'royale with cheese lot', 'pikes peak lot', 'brexit lot']
 }
